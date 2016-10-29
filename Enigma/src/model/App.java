bc4ad93e72a8d1e12ce10a9a3f72c8e427a243f2
AlexanderB92 <a.m.brandstrup@gmail.com>
2016-10-29 02:11:12 -0400

    Added encryption class

--- Enigma/src/model/App.java
+++ Enigma/src/model/App.java
@@ -7,6 +7,11 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		//STARTING ROTATOR POSITIONS
+		int startingPos1 = 2;
+		int startingPos2 = 0;
+		int startingPos3 = 0;
+		
 		//Rotor1
 		int[][] rotor1 = {
 				{2,4},
@@ -14,7 +19,7 @@ public class App {
 				{3,1},
 				{4,2}
 		};
-		Rotator r1 = new Rotator(rotor1, 0);
+		Rotator r1 = new Rotator(rotor1, startingPos1);
 		
 		//Rotor2
 		int[][] rotor2 = {
@@ -23,6 +28,7 @@ public class App {
 				{2,3},
 				{1,4}
 		};
+		Rotator r2 = new Rotator(rotor2, startingPos2);
 		
 		//Rotor3
 		int[][] rotor3 = {
@@ -31,6 +37,7 @@ public class App {
 				{2,4},
 				{3,3}
 		};
+		Rotator r3 = new Rotator(rotor3, startingPos3);
 		
 		//Reflector
 		Map<Character, Character> reflectorMap = new HashMap();
