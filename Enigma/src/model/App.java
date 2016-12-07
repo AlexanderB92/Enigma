2ede885367b4ecd8a62c1da4a809c34d77aaac31
AlexanderB92 <a.m.brandstrup@gmail.com>
2016-12-06 22:59:12 -0500

    added reflector map and first reflector map

--- Enigma/src/model/App.java
+++ Enigma/src/model/App.java
@@ -14,8 +14,8 @@ public class App {
 		
 		//Rotor1
 		int[][] rotor1 = {
-				{2,1,3,0},
-				{0,3,1,2}
+				{2,1,3,0,4,24,6,7,22,9,17,14,12,13,11,15,26,10,21,19,20,18,8,23,5,25,16},
+				{0,19,1,2,25,9,17,8,21,5,26,23,12,15,14,16,13,6,18,3,20,7,22,11,24,4,10}
 		};
 		Rotator r1 = new Rotator(rotor1, startingPos1);
 		
@@ -35,10 +35,32 @@ public class App {
 		
 		//Reflector
 		Map<Character, Character> reflectorMap = new HashMap<Character, Character>();
-		reflectorMap.put('A', 'C');
-		reflectorMap.put('B', 'D');
-		reflectorMap.put('C', 'A');
-		reflectorMap.put('D', 'B');
+		reflectorMap.put('A', 'P');
+		reflectorMap.put('B', 'V');
+		reflectorMap.put('C', 'M');
+		reflectorMap.put('D', 'Z');
+		reflectorMap.put('E', 'I');
+		reflectorMap.put('F', 'T');
+		reflectorMap.put('G', 'O');
+		reflectorMap.put('H', 'J');
+		reflectorMap.put('I', 'E');
+		reflectorMap.put('J', 'H');
+		reflectorMap.put('K', 'L');
+		reflectorMap.put('L', 'K');
+		reflectorMap.put('M', 'C');
+		reflectorMap.put('N', 'Q');
+		reflectorMap.put('O', 'G');
+		reflectorMap.put('P', 'A');
+		reflectorMap.put('Q', 'N');
+		reflectorMap.put('R', 'W');
+		reflectorMap.put('S', 'U');
+		reflectorMap.put('T', 'F');
+		reflectorMap.put('U', 'S');
+		reflectorMap.put('V', 'B');
+		reflectorMap.put('W', 'R');
+		reflectorMap.put('X', 'Y');
+		reflectorMap.put('Y', 'X');
+		reflectorMap.put('Z', 'D');
 		Reflector reflector = new Reflector(reflectorMap);
 		
 		//Encryptor
