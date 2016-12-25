ec10b7240a5a0ebe70bf2ed3972eeaa37fb54ca4
AlexanderB92 <a.m.brandstrup@gmail.com>
2016-12-24 22:06:17 -0500

    removed unused code

--- Enigma/src/model/Encryption.java
+++ Enigma/src/model/Encryption.java
@@ -17,7 +17,9 @@ public class Encryption {
 	}
 
 	public void encrypt(Character letter) {
-
+		
+		int i = 1;
+		i=-1;
 		Character result;
 		int currentIndex;
 		int alphabetIndex = getAlphabetIndex(letter);
